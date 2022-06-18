5cd4ca6333c250237620df2b953ae25158a33ecf
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-06-18 21:26:06 +0200

    redux store

---
+++ client/src/redux/initialState.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    products: {
+      data: [],
+      request: {
+        pending: false,
+        error: null,
+        success: null,
+      },
+    },
+    cart: [],
+    orders: [],
+  };
