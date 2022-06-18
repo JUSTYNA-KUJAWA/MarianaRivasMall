72cec5dcf17cb2aa43334239cd742cc648f214ba
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-06-18 20:07:29 +0200

    initial commit

---
+++ client/src/components/MainLayout/MainLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import styles from './MainLayout.module.scss';
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className={styles.root}>
+      {children}
+    </div>
+  );
+};
+
+MainLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MainLayout;
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const productsRoutes = require('./routes/products.routes');
+const ordersRoutes = require('./routes/orders.routes');
+
+const app = express();
+
+/* MIDDLEWARE */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* API ENDPOINTS */
+app.use('/api', productsRoutes);
+app.use('/api', ordersRoutes);
+
+/* API ERROR PAGES */
+app.use('/api', (req, res) => {
+  res.status(404).send({ page: 'Not found...' });
+});
+
+/* REACT WEBSITE */
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+/* MONGOOSE */
+
+if(NODE_ENV === 'production') dbUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.rt8m7.mongodb.net/mariana?retryWrites=true&w=majority`;
+else dbUri = 'mongodb://localhost:27017/mariana';
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Successfully connected to the database');
+});
+db.on('error', err => {
+  if(process.env.production === true) console.log('there is no connection to db...');
+  else console.log(err);
+});
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
